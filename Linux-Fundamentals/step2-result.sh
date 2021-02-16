c8e1a5d6332ac16a3b5174dffd9b37ca7a9241b9
EricssonBootCamp <abhishek.u.kumar@ericsson.com>
2021-02-17 01:40:15 +0530

    Fourth Update

--- Linux-Fundamentals/step2-result.sh
+++ Linux-Fundamentals/step2-result.sh
@@ -2,7 +2,7 @@
 result=/var/tmp/result2.txt
 clear
 if [ -s "test.txt" ]; then
-if [ `cat test.txt` == "This is Awesome." ]; then
+if [ "`cat test.txt`" == "This is Awesome." ]; then
 echo -e "\n\n\n\t\t\t\tTest Case#2 Passed Successfully" > $result
 else
 echo -e "\n\n\n\t\t\t\tTest Case#2 Failed" >> $result
