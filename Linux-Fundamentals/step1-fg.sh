e701aafee981e83b3baaf4648b5bd270d522ab65
EricssonBootCamp <abhishek.u.kumar@ericsson.com>
2021-02-17 02:08:10 +0530

    Fourth Update

--- Linux-Fundamentals/step1-fg.sh
+++ Linux-Fundamentals/step1-fg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-echo "Checking your Test Case#1 Result"; sleep 1; ./wait.sh;  echo "Done"
+echo "Checking your Test Case#1 Result"; sleep 1; /usr/local/bin/wait.sh;  echo "Done"
 cat /var/tmp/result1.txt
--- Linux-Fundamentals/step2-fg.sh
+++ Linux-Fundamentals/step2-fg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-echo "Checking your Test Case#2 Result"; sleep 1; ./wait.sh;  echo "Done"
+echo "Checking your Test Case#2 Result"; sleep 1; /usr/local/bin/wait.sh;  echo "Done"
 cat /var/tmp/result2.txt
