4cc3b5d76d8b05c624814a2a0afd051c4036a47f
EricssonBootCamp <abhishek.u.kumar@ericsson.com>
2021-02-22 15:28:33 +0530

    5th Update

--- Linux-Fundamentals/step2-fg.sh
+++ Linux-Fundamentals/step2-fg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-echo "Checking your Test Case#2 Result";  sleep 1; wait.sh; echo "Done"
+echo "Checking your Test Case#2 Result";  sleep 15; echo "Done"
 cat /var/tmp/result2.txt
